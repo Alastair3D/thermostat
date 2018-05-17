c9412c1faa139089fa6a58133daec96e2718858c
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-05-17 14:52:47 +0100

    Failing to toggle power mode html

--- interface.js
+++ interface.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
   var thermostat = new Thermostat()
+  $('.temperature').text(thermostat.temperature);
 
-   
+$(".up").click(function(){
+  thermostat.up();
+  $('.temperature').text(thermostat.temperature);
+});
+
+
+$(".down").click(function() {
+  thermostat.down();
+  $('.temperature').text(thermostat.temperature);
+});
+
+$(".toggle-ps-mode").click(function() {
+  if (thermostat.powerSavingIsOn = true) {
+  thermostat.turnPowerSavingOff()
+  $("#Status").text("Normal")
+} else {
+  thermostat.turnPowerSavingOn()
+  $(".power-status").text("Saving")
+}
+
+});
+
 
 
 })
-
-
-$('.temperature').text(30);
